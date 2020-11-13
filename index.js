70fbe6c375abe975a23fc3e0c26cab95c590420a
cmortera <69058940+cmortera@users.noreply.github.com>
2020-11-13 17:00:47 -0500

    Added EJS

--- index.js
+++ index.js
@@ -7,10 +7,17 @@ app.set("view engine", "ejs");
 //joining path where index.js is with /views if starting nodemon outside of that directory
 app.set("views", path.join(__dirname, "/views"));
 
-app.get("/random", (req, res) => {
-  const num = Math.floor(Math.random() * 10) + 1;
-  res.render("random", { rand: num });
+// app.get("/random", (req, res) => {
+//   const num = Math.floor(Math.random() * 10) + 1;
+//   // Render the file name, then key value pair of the variable
+//   res.render("random", { rand: num });
+// });
+
+app.get("/cats", (req, res) => {
+  const cats = ["Blue", "Rocket", "Monty", "Subie"];
+  res.render("cats", { cats });
 });
+
 app.get("/", (req, res) => {
   res.render("home");
 });
